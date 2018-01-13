804b156f95110c9707a9c6e29f28ca921997f406
Boubakr NOUR <nboubakr@users.noreply.github.com>
2018-01-13 17:30:13 +0800

    v2018.2.1.1

---
+++ js/val.js
@@ -0,0 +1,10 @@
+var journalPapers = 0;
+var conferencePapers = 2;
+var totalCitations = 1;
+
+$(document).ready(function () {
+    $("#journalPapers").text(journalPapers);
+    $("#conferencePapers").text(conferencePapers);
+    $("#totalCitations").text(totalCitations);
+});
+
