ffd8b1f5526977ec754baed25eeba666a3337a05
Boubakr NOUR <nboubakr@users.noreply.github.com>
2018-09-27 13:12:39 +0800

    v2018.12.12.1

--- js/val.js
+++ js/val.js
@@ -1,7 +1,7 @@
 var bookChapters = 1;
-var journalPapers = 5;
+var journalPapers = 6;
 var conferencePapers = 7;
-var totalCitations = "10+";
+var totalCitations = "12+";
 
 $(document).ready(function () {
     $("#bookChapters").text(bookChapters);
